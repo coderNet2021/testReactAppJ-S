6f75f0237d33f1d2f9e89385c2406170c4521cd7
Lilyane Fawaz <cnss.saida@gmail.com>
2021-09-11 23:27:30 +0300

    react app by john smilga

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//stateless functional component
+//always return jsx
+
+//this function is capitalized ,
+//it is a COMPONENT!!!
+
+//JSX RULES :
+//always return a single element
+//div section fragment article
+//use camelcase
+//className instead of Class
+//closing every single element
+//formatting
+
+// function Greeting() {
+//   return (
+//     <div>
+//      <Person />
+//      <Message />
+//     </div>
+//   );
+// }
+
+// const Person = () => <h3> lilyane fawaz</h3>;
+
+// const Message = () => {
+//   return <p>this is a message</p>;
+// };
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "hello world!");
+// };
+
+function BoolList() {
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
+}
+
+const author = "Amelia Hepworth";
+const Book = () => {
+  const title = "I Love You to the Moon and Back";
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+        alt=""
+      />
+      <h1> {title}</h1>
+      <h4>{author}</h4>
+      <p>{6 + 6}</p>
+    </article>
+  );
+};
+
+ReactDom.render(<BoolList />, document.getElementById("root"));
---
+++ testing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//rafce snippet
+
+const testing = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default testing
